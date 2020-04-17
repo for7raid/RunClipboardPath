23167ea94de72f4a613d5b1d21254c5a6c17869f
Alexey Karov <akarov@pmpractice.ru>
2020-04-17 18:19:04 +0300

    init

---
+++ RunClipboardPath/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunClipboardPath
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application. d:\temp\1.pdf
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var path = Clipboard.GetText();
+            if (!string.IsNullOrEmpty(path) && (System.IO.Directory.Exists(path) || System.IO.File.Exists(path)))
+                System.Diagnostics.Process.Start(path);
+        }
+    }
+}
